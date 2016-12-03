3f7d11c209dc079b878c3fe8aaa48e14536d031a
ivanheral <ivanhernandezal@gmail.com>
2016-12-03 22:02:39 +0100

    fix styles

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,19 +4,20 @@ import { IndexLink, Link } from 'inferno-router'
 export default function({ children }) {
     return <div>
         <menu className="container">
-            <div className="align-left">
+            <nav className="align-left">
                 <IndexLink>Home</IndexLink>
-            </div>
-            <div className="align-right">
+            </nav>
+            <nav className="align-right">
                 <Link to="/about">About <DownArrow/></Link>
                 <Link to="/docs">Docs <DownArrow/></Link>
                 <Link to="/contribute">Contribute</Link>
                 <Link to="/help">Help</Link>
-            </div>
+            </nav>
         </menu>
         <main>
             {children}
         </main>
+        <footer>footer</footer>
     </div>
 }
 
