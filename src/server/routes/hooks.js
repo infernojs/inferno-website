4f3653e4d08ee958e5c4682b7e2bf068c18ed1db
Ryan Megidov <ryan@megidov.com>
2016-12-07 16:56:28 +0100

    Test push hook #8

--- src/server/routes/hooks.js
+++ src/server/routes/hooks.js
@@ -1,6 +1,4 @@
-import Inferno from 'inferno'
 import fs from 'fs'
-import sys from 'sys'
 import path from 'path'
 import { exec } from 'child_process'
 import crypto from 'crypto'
@@ -9,7 +7,6 @@ import router from 'koa-router'
 export default router()
 .post('/api/hooks', async(ctx, next) => {
     const { fields, headers } = ctx.request
-    console.log('sha1', headers['x-hub-signature'].substr(5))
 
     if (getSecret(JSON.stringify(fields)) === headers['x-hub-signature'].substr(5)) {
         pullAndUpdate()
@@ -24,16 +21,16 @@ function getSecret(body) {
     const INFERNOJS_SECRET = fs.readFileSync(path.join(__dirname, 'INFERNOJS_SECRET'))
     const secret = process.env.INFERNOJS_SECRET || INFERNOJS_SECRET
     const hash = crypto.createHmac('sha1', secret).update(body).digest('hex')
-
-    console.log('SECRET', secret)
-    console.log('HASH', hash)
     return hash
 }
 
 function pullAndUpdate() {
     const cmd = 'cd /www/infernojs; git pull'
-    exec(cmd, function puts(error, stdout, stderr) {
-        fs.writeFileSync(path.join(__dirname, process.env.INFERNOJS_SECRET), 'OK')
-        sys.puts(stdout)
-    });
+    exec(cmd, function(error, stdout, stderr) {
+        if (error) {
+            return console.error(error)
+        }
+        if (stderr) console.error(stderr)
+        console.log(stdout)
+    })
 }
