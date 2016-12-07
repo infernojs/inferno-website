8c6ac9cf46db6be6d46d6ec6c538071584138b61
Ryan Megidov <ryan@megidov.com>
2016-12-07 16:11:10 +0100

    Testing hooks

---
+++ src/server/routes/hooks.js
@@ -0,0 +1,39 @@
+import Inferno from 'inferno'
+import fs from 'fs'
+import path from 'path'
+import router from 'koa-router'
+
+export default router()
+.post('/api/hooks', async(ctx, next) => {
+    const { fields } = ctx.request
+
+    console.log(fields)
+    fs.writeFileSync(path.join(__dirname, 'test----post.json'), JSON.stringify(fields))
+
+    ctx.body = {
+        success: true
+    }
+})
+.put('/api/hooks', async(ctx, next) => {
+    const { fields } = ctx.request
+
+    console.log(fields)
+    fs.writeFileSync(path.join(__dirname, 'test----put.json'), JSON.stringify(fields))
+
+    ctx.body = {
+        success: true
+    }
+})
+.get('/api/hooks', async(ctx, next) => {
+    console.log(ctx.query)
+    fs.writeFileSync(path.join(__dirname, 'test----get.json'), JSON.stringify(ctx.query))
+
+    ctx.body = {
+        success: true
+    }
+})
+
+// FasterThanHell
+async function pullAndUpdate(location) {
+
+}
