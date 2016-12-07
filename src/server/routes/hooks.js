ccf0d95f222a740f99f7d300568579db2bea8a57
Ryan Megidov <ryan@megidov.com>
2016-12-07 16:45:13 +0100

    Test push hook #3

--- src/server/routes/hooks.js
+++ src/server/routes/hooks.js
@@ -1,20 +1,35 @@
 import Inferno from 'inferno'
 import fs from 'fs'
+import sys from 'sys'
 import path from 'path'
+import { exec } from 'child_process'
+import crypto from 'crypto'
 import router from 'koa-router'
 
 export default router()
-.post('/api/hooks', async(ctx, next) => {
+.get('/api/hooks', async(ctx, next) => {
     const { fields, headers } = ctx.request
 
-    fs.writeFileSync(path.join(__dirname, 'test-headers.json'), JSON.stringify(headers, null, 4))
-    fs.writeFileSync(path.join(__dirname, 'test-post.json'), JSON.stringify(fields, null, 4))
+    if (getSecret(JSON.stringify(fields)) === headers['x-hub-signature'].substr(5)) {
+        pullAndUpdate()
+    }
 
     ctx.body = {
         success: true
     }
 })
 
-async function pullAndUpdate(location) {
+function getSecret(body) {
+    const INFERNOJS_SECRET = fs.readFileSync(path.join(__dirname, 'INFERNOJS_SECRET'))
+    const secret = process.env.INFERNOJS_SECRET || INFERNOJS_SECRET
+    return crypto.createHmac('sha1', secret).update(body).digest('hex')
+}
 
+function pullAndUpdate() {
+    // placeholder
+    const cmd = 'cd /www/infernojs; git pull'
+    exec(cmd, function puts(error, stdout, stderr) {
+        fs.writeFileSync(path.join(__dirname, process.env.INFERNOJS_SECRET), 'OK')
+        sys.puts(stdout)
+    });
 }
