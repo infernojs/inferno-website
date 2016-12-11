459e79cb9d957391071688fc675e37038f513d2f
David Di Biase <david@pilotinteractive.ca>
2016-12-11 09:54:21 -0500

    Cleaned up some styling

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,21 +4,20 @@ import IconSlack from '../icons/IconSlack'
 import IconContributors from '../icons/IconContributors'
 
 export default function Footer() {
-    return <footer>
-            <div className="container">
-                <div className="row">
-                    <div className="social-network">
-                        <a target="_blank" href="https://twitter.com/infernojs">
-                            <IconTwitter/>
-                        </a>
-                        <a target="_blank" href="https://inferno-slack.herokuapp.com/">
-<IconSlack/>
-                        </a>
-                    </div>
-                    <div className="mit"><a target="_blank" href="https://opensource.org/licenses/MIT">Project licensed under MIT</a></div>
-                    <div className="contributors"><IconContributors/>
-                         Contributors </div>
+    return (
+    <footer>
+        <div className="container">
+            <div className="row">
+                <div className="social-network">
+                    <a target="_blank" href="https://twitter.com/infernojs"><IconTwitter/></a>
+                    <a target="_blank" href="https://inferno-slack.herokuapp.com/"><IconSlack/></a>
                 </div>
+                <div className="mit">
+                    <a target="_blank" href="https://opensource.org/licenses/MIT">Project licensed under MIT</a>
+                </div>
+                <a className="contributors" href="https://github.com/trueadm/inferno/graphs/contributors"><IconContributors/> Contributors </a>
             </div>
-        </footer>
+        </div>
+    </footer>
+    )
 }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,24 +4,26 @@ import { Link } from 'inferno-router'
 import IconMenu from '../icons/IconMenu'
 
 export default class Header extends Component {
-        constructor(props) {
+    constructor(props) {
         super(props)
         this.state = {active: false}
     }
-
     click() {
-        this.setState({active: !this.state.active});
+        this.setState({ active: ! this.state.active });
     }
-
     render() {
-        return  <div><div className="menu-bar" onClick={this.click.bind(this)}>
-                <IconMenu/>
-                </div><nav className={this.state.active ? "show" :""}>
-                <Link onClick={this.click.bind(this)} to="/about">About</Link>
+        return (
+        <div>
+            <div className="menu-bar" onClick={ this.click.bind(this) }>
+            <IconMenu/>
+            </div>
+            <nav className={ this.state.active ? "show" :"" }>
+                <Link onClick={ this.click.bind(this) } to="/about">About</Link>
                 <Link to="/docs">Docs</Link>
                 <Link to="/contribute">Contribute</Link>
-                <Link to="/help">Help</Link>
-
-        </nav></div>
+                <Link to="https://github.com/trueadm/inferno/issues">Help</Link>
+            </nav>
+        </div>
+        )
     }
 }
