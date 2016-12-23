5dd1324860ba5b2c565e662f440491b0cb890c86
Ryan Megidov <ryan@megidov.com>
2016-12-23 14:38:48 +0100

    Add SPDY support

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,13 @@
 import logger from 'debug'
 import Koa from 'koa'
+import fs from 'fs'
 import bodyParser from 'koa-better-body'
 import compress from 'koa-compress'
 import favicon from 'koa-favicon'
 import mount from 'koa-mount'
 import serve from 'koa-static'
 import convert from 'koa-convert'
+import spdy from 'spdy'
 
 import config from './config'
 import catcher from './middleware/catcher'
@@ -39,6 +41,27 @@ app.use(serve(require('path').join(__dirname, '../assets/service')))
 
 app.use(render)
 
-app.listen(config.http.port, function() {
+const options = {
+    spdy: {
+        procotols: ['h2'],
+        plain: true,
+        ssl: false
+    }
+}
+
+try {
+    if (fs.openSync(config.server.certificate, fs.constants.R_OK)) {
+        options.cert = fs.readFileSync(config.server.certificate)
+        options.key = fs.readFileSync(config.server.certificate_key)
+    }
+    logger('inferno:start')('Running with HTTP/2 enabled')
+} catch(e) {
+    logger('inferno:start')('Running with HTTP/2 disabled')
+}
+
+
+const server = spdy.createServer(options, app.callback())
+
+server.listen(config.http.port, function() {
     logger('inferno:start')('Listening on port ' + config.http.port)
 })
