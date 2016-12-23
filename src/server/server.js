0fa2aca20d62aa5635eaa3b37ec0546c85f5cf7e
Ryan Megidov <ryan@megidov.com>
2016-12-23 18:27:29 +0100

    Attempt #1 at http2 with certs

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import logger from 'debug'
 import Koa from 'koa'
+import fs from 'fs'
 import path from 'path'
 import bodyParser from 'koa-better-body'
 import compress from 'koa-compress'
@@ -7,9 +8,10 @@ import favicon from 'koa-favicon'
 import mount from 'koa-mount'
 import serve from 'koa-static'
 import convert from 'koa-convert'
+import http2 from 'http2'
 
 import config from './config'
-import serverpush from './middleware/serverpush'
+// import serverpush from './middleware/serverpush'
 import catcher from './middleware/catcher'
 import render from './middleware/render'
 import markdown from './routes/markdown'
@@ -18,9 +20,10 @@ import hooks from './routes/hooks'
 const app = new Koa()
 
 // Middleware
-app.use(serverpush({
-    manifestName: path.join(__dirname, '../../push_manifest.json')
-}))
+/*app.use(serverpush({
+    manifest: path.join(__dirname, '../../push_manifest.json'),
+    singleheader: false
+}))*/
 app.use(favicon(config.http.favicon))
 app.use(compress())
 app.use(convert(bodyParser({
@@ -45,6 +48,25 @@ app.use(serve(require('path').join(__dirname, '../assets/service')))
 // Render inferno app
 app.use(render)
 
-app.listen(config.http.port, function() {
-    logger('inferno:start')('Listening on port ' + config.http.port)
-})
+if (process.env.DEV) {
+    // Without HTTP2 support
+    app.listen(config.http.port, function() {
+        logger('inferno:start')('Listening on port ' + config.http.port)
+    })
+} else {
+    // With HTTP2 support
+    const server = http2.createServer({
+        key: fs.readFileSync(config.server.certificate_key),
+        cert: fs.readFileSync(config.server.certificate)
+    }, app.callback())
+
+    server.listen(config.http.port, (error) => {
+        if (error) {
+            logger('inferno:error')(error)
+            process.exit(1)
+        } else {
+            logger('inferno:start')('Running with HTTP/2 enabled')
+            logger('inferno:start')('Listening on port ' + config.http.port)
+        }
+    })
+}
