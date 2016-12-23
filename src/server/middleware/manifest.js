37bf29dee71f52c672dab291bc09404c37772bc0
Ryan Megidov <ryan@megidov.com>
2016-12-23 16:03:56 +0100

    HTTP/2 Push Link implementation

---
+++ src/server/middleware/manifest.js
@@ -0,0 +1,27 @@
+import url from 'url'
+import path from 'path'
+
+export default function manifestMw (manifestFile) {
+    const manifest = require(path.resolve('.', manifestFile));
+
+    return async(ctx, next) => {
+        // If we receive a request for a file _inside_ the manifest, move on to the next middleware.
+        if (manifest[ctx.req.url]) {
+            return next();
+        }
+
+        const links = [];
+
+        Object.keys(manifest).forEach(key => {
+            const u = url.resolve(`${ctx.protocol}://${ctx.host}`, key);
+            const link = `<${u}>; rel=preload; as=${manifest[key].type}`;
+            links.push(link);
+        });
+
+        ctx.set('Link', links.join(', '));
+        // Link: <https://localhost:3000/static/main-9a42a4188a.css>; rel=preload; as=style, <https://localhost:3000/app/common_2-3574809b05.js>; rel=preload; as=script, <https://localhost:3000/static/js/primus.js>; rel=preload; as=script, <https://localhost:3000/bower_components/polymer/polymer.html>; rel=preload; as=document
+        ctx.state.h2 = manifest;
+
+        return await next();
+    }
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,15 @@
 import logger from 'debug'
 import Koa from 'koa'
-import fs from 'fs'
+import path from 'path'
 import bodyParser from 'koa-better-body'
 import compress from 'koa-compress'
 import favicon from 'koa-favicon'
 import mount from 'koa-mount'
 import serve from 'koa-static'
 import convert from 'koa-convert'
-import spdy from 'spdy'
 
 import config from './config'
+import serverpush from './middleware/serverpush'
 import catcher from './middleware/catcher'
 import render from './middleware/render'
 import markdown from './routes/markdown'
@@ -18,6 +18,9 @@ import hooks from './routes/hooks'
 const app = new Koa()
 
 // Middleware
+app.use(serverpush({
+    manifestName: path.join(__dirname, '../../push_manifest.json')
+}))
 app.use(favicon(config.http.favicon))
 app.use(compress())
 app.use(convert(bodyParser({
@@ -39,29 +42,9 @@ config.static.forEach(staticRoute => {
 // Serve service worker
 app.use(serve(require('path').join(__dirname, '../assets/service')))
 
+// Render inferno app
 app.use(render)
 
-const options = {
-    spdy: {
-        procotols: ['h2'],
-        plain: true,
-        ssl: false
-    }
-}
-
-try {
-    if (fs.openSync(config.server.certificate, fs.constants.R_OK)) {
-        options.cert = fs.readFileSync(config.server.certificate)
-        options.key = fs.readFileSync(config.server.certificate_key)
-    }
-    logger('inferno:start')('Running with HTTP/2 enabled')
-} catch(e) {
-    logger('inferno:start')('Running with HTTP/2 disabled')
-}
-
-
-const server = spdy.createServer(options, app.callback())
-
-server.listen(config.http.port, function() {
+app.listen(config.http.port, function() {
     logger('inferno:start')('Listening on port ' + config.http.port)
 })
