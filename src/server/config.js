37c73d10c83027a76eaacec6406fd0eb32555722
Ryan Megidov <ryan@megidov.com>
2016-12-27 22:30:07 +0100

    Re-enable SSR

--- src/server/config.js
+++ src/server/config.js
@@ -6,7 +6,7 @@ module.exports = {
         favicon: path.join(__dirname, '../assets/favicon.ico')
     },
     server: {
-        SSR: false, // Server side rendering
+        SSR: true, // Server side rendering
         certificate: path.join(__dirname, '../../fullchain.pem'),
         certificate_key: path.join(__dirname, '../../privkey.pem'),
     },
