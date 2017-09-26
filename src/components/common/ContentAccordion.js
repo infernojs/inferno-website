32f8f42d14edfaf993c3352e194976fb479b43b1
Ryan Megidov <ryan@starcoders.com>
2017-09-26 11:56:21 +0200

    Updated footer Simplified features texts Made benchmark icon bigger

--- src/components/common/ContentAccordion.js
+++ src/components/common/ContentAccordion.js
@@ -16,13 +16,12 @@ export default class ContentAccordion extends Component {
         {
           name: 'Differences from React',
           description: [
-            "Inferno is much smaller in size, 9kb vs 45kb gzip. This means Inferno is faster to transfer over the network but more importantly, is much faster to parse – this makes a big impact on mobile.",
-            "Inferno is considerably faster than React. This doesn't apply to only benchmarks, but real-world applications that companies have converted to Inferno from React. Ranging from 40% - 110% performance improvement with Inferno 1.0. No other React-like library gets close to this performance gain over React.",
-            "Inferno doesn't have a fully synthetic event system like React does. Inferno has a partially synthetic event system, instead opting to only delegate certain events (such as onClick).",
+            "Inferno is much smaller in size, 9kb vs 45kb gzip. This makes a big impact on mobile.",
+            "Inferno is considerably faster than React. This doesn't apply to only benchmarks, but real-world applications that companies have converted to Inferno from React.",
+            "Inferno doesn't have a fully synthetic event system like React does, instead opting to only delegate certain events (such as onClick).",
             "Inferno doesn't support React Native. Inferno was only designed for the browser/server with the DOM in mind.",
-            "Inferno doesn't support string refs – although this can be enabled using inferno-compat. We don't recommend using them, they are the source of many memory leaks and performance issues in real-world apps. Stick with function callback refs instead.",
-            "Inferno includes render on the main core package, rather than have a InfernoDOM package like React does. We used to do it that way, but we found people simply didn't like it given we don't support native. Furthermore, by not splitting them, we improved performance and bundle sizes.",
-            "Inferno provides lifecycle events on stateless components. This is a major win for people who prefer lightweight components rather than bloated ES2015 classes.",
+            "Inferno doesn't support string refs – although this can be enabled using inferno-compat. We don't recommend using them, they are the source of many memory leaks and performance issues in real-world apps.",
+            "Inferno provides lifecycle events on stateless components. This is a major win for people who prefer lightweight components rather than ES2015 classes.",
             "Inferno is able to use the React Dev Tools extensions for Chrome/Firefox/etc to provide the same level of debugging experience to users of Inferno via `inferno-devtools`"
           ]
         },
@@ -31,22 +30,9 @@ export default class ContentAccordion extends Component {
           description: [
             "Inferno is larger in size, 9kb vs 3kb gzip. This means that Preact should parse faster than Inferno – if only slightly.",
             "Inferno has a partial synthetic event system, resulting in better performance via delegation of certain events.",
-            "Inferno is much faster than Preact in rendering, updating and removing elements from the DOM. Inferno diffs against virtual DOM, rather than the real DOM (except for when loading from server-side rendered content) which means it can make drastic improvements. Unfortunately, diffing against the real DOM has a 30-40% overhead cost in operations.",
+            "Inferno is much faster than Preact in rendering, updating and removing elements from the DOM.",
             "Inferno fully supports controlled components for input/select/textarea elements. This prevents lots of edgecases where the virtual DOM is not the source of truth (it should always be). Preact pushes the source of truth to the DOM itself.",
-            "Inferno provides lifecycle events on stateless components. This is a major win for people who prefer lightweight components rather than bloated ES2015 classes.",
-          ]
-        },
-        {
-          name: 'Other Frameworks',
-          description: [
-            'Inferno is generally smaller (9kb) than most frameworks even when Gzipped. In comparison to other popular libraries: Ember (130kb), Angular2 (119kb) and Vue (18kb).',
-            'Inferno doesn\'t ship with data fetching or model facilities, it focuses on making the UI as fast as possible and leaves the data side to developers.',
-            'Other monolithic libraries typically ship with an array of features that make them easy to use but bloats delivery to end-users.',
-            'Inferno structures its rendering and UI logic into functional and class components, other frameworks may have more opinionated or elaborate structures.',
-            'Inferno uses JSX (with Babel) and Hyperscript to define the structure of a UI. Other libraries may utilize custom templating methods ie. Angular uses more attribute-based binding and Angular expressions. Since Inferno is a Virtual DOM implementation, JSX allows developers to write their markup with flexibility and transforms the markup into a virtualized DOM structure with quick and efficient diffing. This is a key ingredient to it\'s speed.',
-            'Inferno supports isomorphic (or universal) with the use of the inferno-server module. Other application frameworks may struggle with this task due to their structure or require specialized infrastructure to achieve the same result.',
-            'Inferno is extremely well suited for building progressive web applications that respond quickly and efficiently to user interaction.',
-            'Inferno is not affiliated with any specific organization. It is released under MIT license and supported by a strong open-source community.'
+            "Inferno provides lifecycle events on stateless components. This is a major win for people who prefer lightweight components rather than ES2015 classes.",
           ]
         }
       ]
--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -1,37 +1,37 @@
 import Inferno from 'inferno'
-import IconArchitecture from '../icons/IconArchitecture'
-import IconCompatible from '../icons/IconCompatible'
-import IconIsomorphic from '../icons/IconIsomorphic'
-import IconModular from '../icons/IconModular'
-import IconSize from '../icons/IconSize'
-import IconSpeed from '../icons/IconSpeed'
+// import IconArchitecture from '../icons/IconArchitecture'
+// import IconCompatible from '../icons/IconCompatible'
+// import IconIsomorphic from '../icons/IconIsomorphic'
+// import IconModular from '../icons/IconModular'
+// import IconSize from '../icons/IconSize'
+// import IconSpeed from '../icons/IconSpeed'
 
 export default function() {
   return <div className="container">
     <div className="row features-wrapper">
       <div className="xs6 lg6 row">
-        <IconCompatible/>
+        {/*<IconCompatible/>*/}
         <div className="desc">
           <h2>React Compatible</h2>
           <p>React-like API, concepts and component lifecycle events. Switch over easily with inferno-compat.</p>
         </div>
       </div>
       <div className="xs6 lg6 row">
-        <IconSpeed/>
+        {/*<IconSpeed/>*/}
         <div className="desc">
           <h2>Insane Performance</h2>
           <p>One of the fastest front-end frameworks for rendering UI in the DOM, making 60 FPS on mobile possible.</p>
         </div>
       </div>
       <div className="xs6 lg6 row">
-        <IconArchitecture/>
+        {/*<IconArchitecture/>*/}
         <div className="desc">
           <h2>One-way Architecture</h2>
           <p>Component driven + One-way data flow architecture. Bindings also supplied for Redux, MobX and Cerebral.</p>
         </div>
       </div>
       <div className="xs6 lg6 row">
-        <IconIsomorphic/>
+        {/*<IconIsomorphic/>*/}
         <div className="desc">
           <h2>Isomorphic</h2>
           <p>Isomorphic rendering on both client and server, along with fast-booting from server-side renders.</p>
