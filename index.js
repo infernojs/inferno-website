7ae67232727bb0e39fcdd583254b23a77316e185
aretecode <aretecode@gmail.com>
2019-05-29 16:50:39 -0700

    merge start back into index for command change of babel

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ require('./core/compile');
 global.window = global.window || {};
 
 require('./start')
+/**
+ * Bootstrap our server
+ * 
+ * @todo should compile first, or just use direct `require`
+ */
+require("@babel/register");
+require('./src/server/server');
--- start.js
+++
@@ -1,7 +0,0 @@
-/**
- * Bootstrap our server
- * 
- * @todo should compile first, or just use direct `require`
- */
-require("@babel/register");
-require('./src/server/server');
